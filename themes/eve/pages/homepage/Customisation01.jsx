6bfcede156391b1612668a56e14ef8173e70d3b9
Thokozani Msebele <thoko@byosoftware.co.zw>
2024-10-30 03:46:34 +0200

    first commit

---
+++ themes/eve/pages/homepage/Customisation01.jsx
@@ -0,0 +1,168 @@
+import React from "react";
+import "./Customisation01_01.scss";
+import "./Customisation01_02.scss";
+
+function Customisation01() {
+  return (
+    <div className="u-body u-xl-mode">
+      <section
+        className="u-clearfix u-container-align-left-lg u-container-align-left-xl u-image u-shading u-section-1"
+        id="carousel_7c2c"
+        data-image-width="1500"
+        data-image-height="850"
+      >
+        <div className="u-clearfix u-sheet u-valign-middle u-sheet-1">
+          <div className="data-layout-selected u-clearfix u-expanded-width-xs u-layout-wrap u-layout-wrap-1">
+            <div className="u-layout">
+              <div className="u-layout-row">
+                <div className="u-align-left u-black u-container-style u-layout-cell u-left-cell u-opacity u-opacity-50 u-radius-50 u-shape-round u-size-60 u-layout-cell-1">
+                  <div className="u-container-layout u-container-layout-1">
+                    <h6 className="u-align-center u-custom-font u-font-montserrat u-text u-text-body-alt-color u-text-1">
+                      GET MORE THIS
+                    </h6>
+                    <h2 className="u-align-center u-custom-font u-text u-text-2">
+                      Black Friday
+                    </h2>
+                    <div className="u-container-style u-custom-color-1 u-group u-group-1">
+                      <div className="u-container-layout u-container-layout-2">
+                        <p className="u-align-center u-custom-font u-font-oswald u-text u-text-3">
+                          <span style={{ fontWeight: 700 }}>UP TO 75% OFF</span>
+                        </p>
+                        <p className="u-align-center u-text u-text-4">
+                          Until 7 December
+                        </p>
+                      </div>
+                    </div>
+                    <h4 className="u-align-center u-custom-font u-font-oswald u-text u-text-body-alt-color u-text-5">
+                      ORDER Online NOW
+                    </h4>
+                    <a
+                      href="#"
+                      className="u-active-white u-align-center u-border-none u-btn u-btn-round u-button-style u-custom-font u-font-oswald u-hover-white u-palette-2-base u-radius-50 u-text-active-palette-2-base u-text-body-alt-color u-text-hover-palette-2-base u-btn-1"
+                    >
+                      Shop Now
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="u-clearfix u-section-2" id="carousel_115f">
+        <div className="u-clearfix u-sheet u-valign-middle u-sheet-1">
+          <div className="custom-expanded data-layout-selected u-clearfix u-gutter-0 u-layout-wrap u-layout-wrap-1">
+            <div className="u-layout">
+              <div className="u-layout-row">
+                <div className="u-container-style u-layout-cell u-size-17 u-layout-cell-1">
+                  <div className="u-container-layout u-container-layout-1">
+                    <h4 className="u-custom-font u-font-raleway u-text u-text-custom-color-1 u-text-1">
+                      Exclusive Black Friday Deals
+                    </h4>
+                  </div>
+                </div>
+                <div className="u-container-style u-layout-cell u-size-21 u-layout-cell-2">
+                  <div className="u-container-layout u-container-layout-2">
+                    <ul className="u-custom-list u-spacing-10 u-text u-text-2">
+                      <li>
+                        <div className="u-list-icon u-text-black">
+                          <svg
+                            className="u-svg-content"
+                            viewBox="0 0 512 512"
+                            id="svg-311a"
+                          >
+                            <path
+                              d="m433.1 67.1-231.8 231.9c-6.2 6.2-16.4 6.2-22.6 0l-99.8-99.8-78.9 78.8 150.5 150.5c10.5 10.5 24.6 16.3 39.4 16.3 14.8 0 29-5.9 39.4-16.3l282.7-282.5z"
+                              fill="currentColor"
+                            ></path>
+                          </svg>
+                        </div>
+                        <span style={{ fontWeight: 700 }}>
+                          <span style={{ fontSize: "1.125rem" }}>
+                            Up to 50% off on gas stoves
+                            <br />
+                          </span>
+                        </span>
+                      </li>
+                      <li>
+                        <div className="u-list-icon u-text-black">
+                          <svg
+                            className="u-svg-content"
+                            viewBox="0 0 512 512"
+                            id="svg-311a"
+                          >
+                            <path
+                              d="m433.1 67.1-231.8 231.9c-6.2 6.2-16.4 6.2-22.6 0l-99.8-99.8-78.9 78.8 150.5 150.5c10.5 10.5 24.6 16.3 39.4 16.3 14.8 0 29-5.9 39.4-16.3l282.7-282.5z"
+                              fill="currentColor"
+                            ></path>
+                          </svg>
+                        </div>
+                        <span style={{ fontSize: "1.125rem" }}>
+                          Up to 30% off on dining suites
+                          <br />
+                        </span>
+                      </li>
+                      <li>
+                        <div className="u-list-icon u-text-black">
+                          <svg
+                            className="u-svg-content"
+                            viewBox="0 0 512 512"
+                            id="svg-311a"
+                          >
+                            <path
+                              d="m433.1 67.1-231.8 231.9c-6.2 6.2-16.4 6.2-22.6 0l-99.8-99.8-78.9 78.8 150.5 150.5c10.5 10.5 24.6 16.3 39.4 16.3 14.8 0 29-5.9 39.4-16.3l282.7-282.5z"
+                              fill="currentColor"
+                            ></path>
+                          </svg>
+                        </div>
+                        <span style={{ fontSize: "1.125rem" }}>
+                          Up to 25% off on freezers
+                          <br />
+                        </span>
+                      </li>
+                      <li>
+                        <div className="u-list-icon u-text-black">
+                          <svg
+                            className="u-svg-content"
+                            viewBox="0 0 512 512"
+                            id="svg-311a"
+                          >
+                            <path
+                              d="m433.1 67.1-231.8 231.9c-6.2 6.2-16.4 6.2-22.6 0l-99.8-99.8-78.9 78.8 150.5 150.5c10.5 10.5 24.6 16.3 39.4 16.3 14.8 0 29-5.9 39.4-16.3l282.7-282.5z"
+                              fill="currentColor"
+                            ></path>
+                          </svg>
+                        </div>
+                        <span style={{ fontSize: "1.125rem" }}>
+                          Up to 15% off on fridges
+                        </span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="u-container-style u-grey-5 u-layout-cell u-shape-rectangle u-size-22 u-layout-cell-3">
+                  <div className="u-container-layout u-container-layout-3">
+                    <p className="u-text u-text-3">
+                      Don't miss out on these incredible savings! Our Black
+                      Friday sale offers unbeatable discounts on a wide range of
+                      appliances. Whether you're upgrading your kitchen or
+                      furnishing your dining room, now is the perfect time to
+                      shop and save.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Customisation01;
+
+export const layout = {
+  areaId: "content",
+  sortOrder: 1,
+};
